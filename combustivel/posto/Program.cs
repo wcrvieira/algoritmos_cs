58834e5427554053b7a5470ff89360502fa1c5a9
wcrvieira <wagner.vieira01@cps.sp.gov.br>
2026-05-18 12:35:07 -0300

    Resolução exercício 8

---
+++ combustivel/posto/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Algoritmo que recebe um menu de opções de combustíveis e conta
+    quantos clientes abasteceram em cada opção */
+// Declaração das variáveis
+int opcao = 0;
+int alcool = 0, gasolina = 0, diesel = 0;
+while (opcao != 4)
+{
+    Console.WriteLine("Menu de combustíveis");
+    Console.Write("Digite 1 para Alcool" + "\n" +
+                      "Digite 2 para Gasolina" + "\n" +
+                      "Digite 3 para Diesel" + "\n" +
+                      "Digite 4 para encerrar o sistema!");
+    opcao = Convert.ToInt16(Console.ReadLine());
+    
+    switch (opcao)
+    {
+        case 1: alcool++; break; // alcool =+1 ou alcool = alcool + 1
+        case 2: gasolina++; break; // gasolina += 1 ou gasolina = gasolina + 1
+        case 3: diesel++; break; // diesel += 1 ou diesel = diesel + 1
+        case 4:
+            Console.WriteLine("Fim. Obrigado pela preferência!" + "\n" +
+                                  "Total de veículos com Alcool: " + alcool + "\n" +
+                                  "Total de veículos com Gasolina: " + gasolina + "\n" +
+                                  "Total de veículos com Diesel: " + diesel);
+            break;
+        default: Console.WriteLine("Código inválido"); break;
+    }
+}
---
+++ senhas/senha/Program.cs
@@ -0,0 +1,19 @@
+﻿// Algoritmo que testa senhas
+// Declaração das variáveis
+string senha = "@2026";
+string password = "";
+
+while (senha != password)
+{
+    Console.Write("Digite a senha do sistema: ");
+    password = Console.ReadLine();
+
+    /*if (senha == password) { Console.WriteLine("Acesso permitido!!!");}
+        else {Console.WriteLine("Acesso negado!!!!!!");} */
+    
+    switch (password)
+    {
+        case "@2026": Console.WriteLine("Acesso permitido!!!"); break;
+        default: Console.WriteLine("Acesso negado!"); break;
+    }
+}
