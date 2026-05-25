937009330ad1e58b96a58974abd966d2157f1171
wcrvieira <wagner.vieira01@cps.sp.gov.br>
2026-05-25 13:57:09 -0300

    Novos exercícios

---
+++ impares/Impares.cs
@@ -0,0 +1,23 @@
+﻿// Algoritmo que exibe os números impares entre 1 até
+// o valor digitado pelo usuário
+// Declaração das variáveis
+int x = 1;
+int valor = 0;
+
+Console.Write("Por favor, digite um valor inteiro entre 1 e 1000: ");
+valor = Convert.ToInt16(Console.ReadLine());
+
+if (valor <= 1000)
+{
+    while (x <= valor) {
+        if (x % 2 == 0)
+            {
+                Console.WriteLine($"Valor: {x}");                
+            }
+            x++;
+        }
+}
+else
+{
+    Console.WriteLine("Número inválido!!!!");
+}
---
+++ login/Login.cs
@@ -0,0 +1,38 @@
+﻿// Algoritmo que testa o login e senha de um usuário
+// e tentativas
+// Declaração das variáveis
+string senha = "Hoje2505#";
+string usuario = "root";
+int contador = 0;
+
+while (contador < 3) // Condição para exibir as perguntas
+{
+    if (contador == 3) // Compara o contador com o nº de tentativas
+    {
+       Console.WriteLine("Número de tentativas excedido!!!"); 
+    }
+
+    // Interação com o usuário
+    Console.Write("Informe o usuário: ");
+    string user = Console.ReadLine();
+
+    Console.Write("Digite a senha: ");
+    string password = Console.ReadLine();
+
+    if (usuario == user & senha == password)
+    {
+        Console.WriteLine("Seja bem-vindo ao sistema");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Acesso negado");
+    }
+    contador = contador + 1; // contador += 1; contador++;
+
+    if (contador == 3)
+    {
+       Console.WriteLine("Número de tentativas excedido!!!"); 
+    }
+    
+}
